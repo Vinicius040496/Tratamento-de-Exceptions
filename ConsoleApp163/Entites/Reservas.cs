0a752fb96c2842835f1dd271ce4e08b671108228
Vinicius040496 <vinicius.almeida2011@gmail.com>
2024-07-08 19:25:56 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp163/Entites/Reservas.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp163.Entites.Exception;
+
+namespace ConsoleApp163.Entites
+{
+    internal class Reservas
+    {
+        public int NumeroQuarto { get; set; }
+        public DateTime Entrada { get; set; }
+        public DateTime Saida { get; set; }
+
+        public Reservas()
+        {
+
+        }
+
+        public Reservas(int numeroQuarto, DateTime entrada, DateTime saida)
+        {
+            if (saida < entrada)
+            {
+                throw new DominioException("Erro, data de saida precisa ser posterior a data de entrada!");
+            }
+            NumeroQuarto = numeroQuarto;
+            Entrada = entrada;
+            Saida = saida;
+        }
+
+        public int Duracao()
+        {
+            TimeSpan duração = Saida.Subtract(Entrada);
+            return (int)duração.TotalDays;
+        }
+        public void AtualizarDados(DateTime novaentrada , DateTime novasaida)
+        {
+            DateTime now = DateTime.Now;
+
+            if (novasaida < novaentrada)
+            {
+                throw new DominioException("Erro, nova data de agendamento precisa se posterior a antiga reserva!!");
+            }if (novaentrada < now || novasaida < now)
+            {
+                throw new DominioException("Não é possivel atualizar datas passadas, necessita de datas futuras!!");
+            }
+
+            Entrada = novaentrada;
+            Saida= novasaida;
+                
+        }
+     
+        public override string ToString()
+        {
+            return "Quarto: " + NumeroQuarto + ", " + "Data de entrada: " + Entrada.ToString("dd/MM/yyyy")
+                + ", " + "Data de saida: " + Saida.ToString("dd/MM/yyyy") + ", " + Duracao().ToString()
+                + " Noites ";
+        }
+    }
+}
---
+++ ConsoleApp163/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp163.Entites;
+using ConsoleApp163.Entites.Exception;
+
+namespace ConsoleApp163
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Numero do quarto: ");
+                int numeroQuarto = int.Parse(Console.ReadLine());
+                Console.Write("Data de entrada (dd/mm/yyyy): ");
+                DateTime entrada = DateTime.Parse(Console.ReadLine());
+                Console.Write("Data de saida (dd/mm/yyyy): ");
+                DateTime saida = DateTime.Parse(Console.ReadLine());
+
+                Reservas reservas = new Reservas(numeroQuarto, entrada, saida);
+                Console.WriteLine(reservas);
+
+                Console.WriteLine();
+                Console.WriteLine("Entre com dados para atualizar a reserva: ");
+                Console.Write("Data de entrada (dd/mm/yyyy): ");
+                DateTime novaentrada = DateTime.Parse(Console.ReadLine());
+                Console.Write("Data de saida (dd/mm/yyyy): ");
+                DateTime novasaida = DateTime.Parse(Console.ReadLine());
+
+                reservas.AtualizarDados(novaentrada, novasaida);
+                Console.WriteLine(reservas);
+
+            }
+            catch (DominioException e)
+            {
+                Console.WriteLine("Erro na reserva" + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro inesperado!" + e.Message);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
